3944b1c09e5d062be52d7e4690c153fd7650f891
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2021-01-23 04:41:17 -0800

    update description

--- js/process.js
+++ js/process.js
@@ -3,19 +3,19 @@ const projectGroup = document.querySelector('.project-group');
 const data = [
     {
         "typeOfProject":"Portfolio ",
-        "shortDescription":"Portfolio  site built in HTML, CSS, JavaScript",
+        "shortDescription":"Portfolio  site (HTML, CSS, JavaScript)",
         "image": "https://i.pinimg.com/originals/44/83/37/448337fd82e8c13de37548a9e7ca1261.jpg",
         "projectLink": "https://michaelkasingye.netlify.app/"      
     },
   {
-        "typeOfProject":"Jumia Clone Mini",
-        "shortDescription":"Front-end Ecommerce web App",
+        "typeOfProject":"E-commerce",
+        "shortDescription":"Jumia Clone - Mini (React JS)",
         "image": "https://i.pinimg.com/564x/dc/51/00/dc5100a29f67ce40f4972172e3a8c236.jpg",
         "projectLink": "https://jumia-clone-4fdfe.web.app/"      
     },
     {
         "typeOfProject":"Weather",
-        "shortDescription":"Weather Forecast App Built in React",
+        "shortDescription":"Weather Forecast App (React JS)",
         "image": "https://i.pinimg.com/originals/41/25/00/412500dd8a3c60b3d754ac12c9242709.png",
         "projectLink": "https://weatherforcastapp.netlify.app/"      
     },
