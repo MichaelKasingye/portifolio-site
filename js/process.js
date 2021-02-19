30596a1d76ab9393d8af7f03816660294ba84d1a
MICHAEL KASINGYE <michaelkasingye@gmail.com>
2021-02-19 05:38:15 -0800

    Update process.js

--- js/process.js
+++ js/process.js
@@ -9,13 +9,13 @@ const data = [
     },
   {
         "typeOfProject":"E-commerce",
-        "shortDescription":"Jumia Clone - Mini (React JS)",
+        "shortDescription":"Jumia Clone - Mini (React-Contex API JS)",
         "image": "https://i.pinimg.com/564x/dc/51/00/dc5100a29f67ce40f4972172e3a8c236.jpg",
         "projectLink": "https://jumia-clone-4fdfe.web.app/"      
     },
     {
         "typeOfProject":"Weather",
-        "shortDescription":"Weather Forecast App (React-Contex API JS)",
+        "shortDescription":"Weather Forecast App (React JS)",
         "image": "https://i.pinimg.com/originals/41/25/00/412500dd8a3c60b3d754ac12c9242709.png",
         "projectLink": "https://weatherforcastapp.netlify.app/"      
     },
